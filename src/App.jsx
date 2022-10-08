ebaf8a3b626b5d54392447ec6471dadbc05950ef
loory <ece12akinci@gmail.com>
2022-10-08 16:08:22 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-import React, { useState } from "react";
-import Form from "./components/Form"
-import List from "./components/List";
-import Header from "./components/Header";
-/*This is parent component*/
+import { Link, Route, Routes} from 'react-router-dom'
 
 function App() {
 
-    const [inputText, setInputText] = useState('')
-    const [todos, setTodos] = useState([])
-
     return (
-        <div className="flex flex-col w-96 m-auto bg-gray-100">
-            <Header/>
-            <div className="bg-white rounded-lg drop-shadow-lg shadow-gray-400/50">
-                <Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos}/>
-                <List todos={todos} setTodos={setTodos}/>
-            </div>
-
-        </div>
+        <>
+        </>
     )
 }
 export default App
